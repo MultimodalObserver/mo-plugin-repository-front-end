e51063ba99b3bc8a22094d34427ddd58e8e9e0db
Felo Vilches <felovilches@gmail.com>
2017-09-08 11:53:30 -0300

    funciona el enrutador (simple)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,36 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: Page1Component },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
